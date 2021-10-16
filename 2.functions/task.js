e9911abb403892bae08bc25f6e1e76dccace62fe
Oxana Galyamova <mrs.oxas@gmail.com>
2021-10-17 03:39:01 +0500

    first version

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,72 @@
+'use strict';
+
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg;
+  // Проверяем элементы масива и находим сумму элементов
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] > max){
+      max = arr[i];
+    };
+    if(arr[i] < min){
+      min = arr[i];
+    };
+    sum += arr[i];
 
-  // Ваш код
+  };
+  // Находим среднее значение 
+  avg = sum / arr.length;
+  avg = parseFloat(avg.toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  // Находим сумму элементов
+  for(let i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
+  
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = 0;
+  let sumOfArr = 0;
+  // Вычисляем сумму массива
+    for(let i = 0; i < arrOfArr.length; i++){
+      sumOfArr = func(arrOfArr[i]);
+  // Поверяем условие
+      if(sumOfArr > max){
+        max = sumOfArr;
+      }
+    }
+    
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = Infinity;
+  let max = -Infinity;
+  let diff;
+  // Проверяем элементы массива
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] > max){
+      max = arr[i];
+    };
+    if(arr[i] < min){
+      min = arr[i];
+    };
+    // Находим разницу
+    diff = max - min;
+
+  };
+
+  return diff;
 }
