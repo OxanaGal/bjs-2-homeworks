7c6b9b87807fb105b3eea966d49c1e1fdd53eafd
Oxana Galyamova <mrs.oxas@gmail.com>
2021-10-25 06:53:46 +0500

    version 1.0

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
+  let result = arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
 
   return result; // boolean
 }
 
-function advancedFilter(arr) {
-  let resultArr;
+//const compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
 
-  // Ваш код
+function advancedFilter(arr) {
+  let resultArr = arr.filter((n) => n > 0).filter((n) => n % 3 === 0).map((n) => n * 10);
 
   return resultArr; // array
 }
+
+//const advancedFilter = (arr) => arr.filter((n) => n > 0).filter((n) => n % 3 === 0).map((n) => n * 10);
