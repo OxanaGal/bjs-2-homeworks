03254f4efb715266b6d9642e8d25274e2a206d74
Oxana Galyamova <mrs.oxas@gmail.com>
2021-11-04 01:28:02 +0500

    version 1.0

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,48 @@
-function Student(name, gender, age) {
-    // Ваш код
+'use strict';
 
+function Student(name, gender, age) {
+
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+let student1 = new Student('Jane', 'female', '18');
+let student2 = new Student('John', 'male', '19');
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark){
+  
+  if(this.marks === undefined){ 
+    this.marks = []; 
+  }
+
+  this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function (...moreMarks){
+
+  if(this.marks === undefined){ 
+    this.marks = []; 
+  }
+
+  moreMarks.forEach(mark => this.marks.push(mark));
+}
+
+Student.prototype.getAverage = function (){
+
+  const sumMarks = this.marks.reduce((a, b) => a + b);
+  
+  return sumMarks / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason){
+  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
